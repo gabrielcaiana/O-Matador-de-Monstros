29b829565f0b176178e6e3bab9ce3d822a0f7f33
Gabriel Caiana <gabrielcaiana@hotmail.com>
2019-06-27 23:16:00 -0300

    criando mensagem de win ou lose

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 new Vue ({
     el: '#app',
     data: {
-        playerLife: 19, // Life do jogador
-        monsterLife: 21, // Life do Monstro
+        playerLife: 23, // Life do jogador
+        monsterLife: 0, // Life do Monstro
     },
     computed: {
-
+        hasResult() {
+            //essa função apenas me dará retorno caso o valor de playerLife ou MonsterLife seja 0
+            return this.playerLife == 0 || this.monsterLife == 0
+        }
     },
     methods: {
-
+       
     },
     watch: {
 
